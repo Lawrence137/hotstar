4ee4ecab3fb1e36b8b834beabd33d94293cccec8
Lawrence Kimani <lawrencekimani18@gmail.com>
2025-10-21 12:48:11 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hotstar
+    </h1>
+    </>
+  )
+}
+
+export default App
